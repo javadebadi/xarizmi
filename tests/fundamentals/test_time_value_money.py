954f9a5babe1d1f72e812cf5d6e245f61318c719
javadebadi <javad.ebadi.1990@gmail.com>
2024-12-24 15:54:45 -0700

    update

--- tests/fundamentals/test_time_value_money.py
+++ tests/fundamentals/test_time_value_money.py
@@ -1,5 +1,6 @@
 import pytest
 
+from xarizmi.fundamentals.time_value_money import risk_free_return
 from xarizmi.fundamentals.time_value_money import time_value_money
 
 
@@ -28,7 +29,7 @@ class TestTimeValueMoney:
                 0.05,
                 0.10,
                 0.045,
-                2069,
+                0.2069,
                 id="impatience_to_consume + inflation + risk - xarizmi example",  # noqa: E501
             ),
         ],
@@ -44,4 +45,28 @@ class TestTimeValueMoney:
             impatience_to_consume=impatience_to_consume,
             inflation=inflation,
             risk=risk,
-        ) == pytest.approx(expected_time_value_money, rel=0.001)
+        ) == pytest.approx(expected_time_value_money, rel=0.01)
+
+    @pytest.mark.parametrize(
+        [
+            "impatience_to_consume",
+            "inflation",
+            "expected_risk_free_return",
+        ],
+        [
+            pytest.param(0.01, 0, 0.01, id="impatience_to_consume"),
+            pytest.param(
+                0.02, 0.03, 0.0506, id="impatience_to_consume + inflation"
+            ),
+        ],
+    )
+    def test_risk_free_return(
+        self,
+        impatience_to_consume: float,
+        inflation: float,
+        expected_risk_free_return: float,
+    ) -> None:
+        assert risk_free_return(
+            impatience_to_consume=impatience_to_consume,
+            inflation=inflation,
+        ) == pytest.approx(expected_risk_free_return, rel=0.01)
